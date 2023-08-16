5cc300f10b2097ec202dd497b66215c79ba5a3e8
Disha <bidisha2125@gmail.com>
2023-08-17 00:16:50 +0530

    Done with the post

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const { connectToMongoDb } = require('./config/db');
+const urlRoute = require('./routes/url');
+
 
 const app = express();
 const PORT = 8001;
 
+//database connection
+connectToMongoDb("mongodb://127.0.0.1:27017/short_url").then(() => 
+    console.log("mongodb connected")
+)
+
+//middleware
+app.use(express.json());
+
+//routes
+app.use('/url', urlRoute);
+
+//server
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
