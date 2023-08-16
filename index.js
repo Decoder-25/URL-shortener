02facce94114c27b74950221adfd6c172fe1324d
Disha <bidisha2125@gmail.com>
2023-08-17 00:23:35 +0530

    Done with the uRL shortner

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { connectToMongoDb } = require('./config/db');
 const urlRoute = require('./routes/url');
+const URL = require("./models/url");
 
 
 const app = express();
@@ -16,6 +17,23 @@ app.use(express.json());
 
 //routes
 app.use('/url', urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId,
+        },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            },
+        }
+    );
+    res.redirect(entry.redirectURL);
+});
 
 //server
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
